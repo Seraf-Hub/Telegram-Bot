73dcb0642be4c6bd3a65b0cf9105a2dcaffb5ffd
Seraf-Hub <a.team@hotmail.it>
2026-03-31 08:58:11 +0200

    upload files bot

--- products.js
+++ products.js
@@ -1,34 +1,82 @@
 module.exports = [
   {
-    title: "Cuffie Bluetooth Wireless",
-    price: "29.99",
-    oldPrice: "49.99",
-    link: "https://www.amazon.it/dp/B0C7QX1XYZ",
-    image: "https://picsum.photos/600/400",
-    category: "tech"
-  },
-  {
-    title: "Smartwatch Sport",
+    title: "Cuffie Bluetooth JBL Tune 510BT Wireless",
     price: "39.99",
-    oldPrice: "69.99",
-    link: "https://www.amazon.it/dp/B0B123ABCD",
-    image: "https://picsum.photos/600/400",
+    oldPrice: "59.99",
+    link: "https://www.amazon.it/dp/B08WM3LMJF",
+    image: "https://m.media-amazon.com/images/I/61kFL7ywsZS._AC_SL1500_.jpg",
     category: "tech"
   },
   {
-    title: "Lampada LED Moderna",
+    title: "Auricolari Xiaomi Redmi Buds 4 Lite",
+    price: "24.99",
+    oldPrice: "39.99",
+    link: "https://www.amazon.it/dp/B0BKKY7R6R",
+    image: "https://m.media-amazon.com/images/I/51gq2Wk9zDL._AC_SL1500_.jpg",
+    category: "tech"
+  },
+  {
+    title: "Smartwatch Uomo Donna Fitness Tracker",
+    price: "34.99",
+    oldPrice: "69.99",
+    link: "https://www.amazon.it/dp/B09ZLQ9Y1K",
+    image: "https://m.media-amazon.com/images/I/61M8Jr7G5IL._AC_SL1500_.jpg",
+    category: "tech"
+  },
+  {
+    title: "Powerbank 20000mAh Ricarica Rapida",
+    price: "22.99",
+    oldPrice: "39.99",
+    link: "https://www.amazon.it/dp/B08YJQ8Z6P",
+    image: "https://m.media-amazon.com/images/I/71VbHaAqbML._AC_SL1500_.jpg",
+    category: "tech"
+  },
+  {
+    title: "Striscia LED RGB 10 Metri con Telecomando",
     price: "19.99",
-    oldPrice: "34.99",
-    link: "https://www.amazon.it/dp/B0A456EFGH",
-    image: "https://picsum.photos/600/400",
+    oldPrice: "29.99",
+    link: "https://www.amazon.it/dp/B08QZ7XW5H",
+    image: "https://m.media-amazon.com/images/I/71pP6Z8YyKL._AC_SL1500_.jpg",
     category: "home"
   },
   {
-    title: "Organizer da scrivania",
-    price: "14.99",
-    oldPrice: "24.99",
-    link: "https://www.amazon.it/dp/B0Z789IJKL",
-    image: "https://picsum.photos/600/400",
+    title: "Lampada LED da Scrivania Touch Dimmerabile",
+    price: "18.99",
+    oldPrice: "34.99",
+    link: "https://www.amazon.it/dp/B07YF6F3WL",
+    image: "https://m.media-amazon.com/images/I/61z9kQw8jEL._AC_SL1500_.jpg",
     category: "home"
+  },
+  {
+    title: "Organizer Armadio Salvaspazio Vestiti",
+    price: "16.99",
+    oldPrice: "26.99",
+    link: "https://www.amazon.it/dp/B08L3X7Z8F",
+    image: "https://m.media-amazon.com/images/I/71xQW5R7ZTL._AC_SL1500_.jpg",
+    category: "home"
+  },
+  {
+    title: "Frullatore Portatile USB Smoothie",
+    price: "21.99",
+    oldPrice: "39.99",
+    link: "https://www.amazon.it/dp/B08Z7Y3H9N",
+    image: "https://m.media-amazon.com/images/I/61g8QX6jFPL._AC_SL1500_.jpg",
+    category: "home"
+  },
+  {
+    title: "Mini Aspirapolvere Portatile Auto",
+    price: "25.99",
+    oldPrice: "45.99",
+    link: "https://www.amazon.it/dp/B09JZQ8Y7X",
+    image: "https://m.media-amazon.com/images/I/71k3X8Z6YPL._AC_SL1500_.jpg",
+    category: "tech"
+  },
+  {
+    title: "Supporto Smartphone Auto Universale",
+    price: "13.99",
+    oldPrice: "24.99",
+    link: "https://www.amazon.it/dp/B07V3Z8X9T",
+    image: "https://m.media-amazon.com/images/I/61Yx5zLQZSL._AC_SL1500_.jpg",
+    category: "tech"
   }
 ];
